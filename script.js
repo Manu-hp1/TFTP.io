75c44e325ce927006a3df7ee0aee8c7c0d04ee7c
Manu H P <manuhp012@gmail.com>
2026-03-22 22:31:13 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById("menuToggle");
-const navButtons = document.getElementById("navButtons");
-
-toggle.addEventListener("click", () => {
-    navButtons.classList.toggle("active");
-});
